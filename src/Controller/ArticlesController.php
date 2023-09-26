8679deead1ec1cfaf9a06f0e1be99179200a02fe
thomasdindin <thomas.dindin@gmail.com>
2023-09-26 16:27:51 +0200

    modif affichage des articles

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoriesRepository;
+use App\Repository\ProduitsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,10 +19,12 @@ class ArticlesController extends AbstractController
     }
 
     #[Route('/articles/{filtre}', name: 'app_articles_filtre')]
-    public function showArticleFiltre(String $filtre): Response{
+    public function showArticleFiltre(String $filtre, CategoriesRepository $categories, ProduitsRepository $produits): Response{
         return $this->render('articles/articles.html.twig', [
             'controller_name' => 'ArticlesController',
-            'filtre' => $filtre
+            'filtre' => $filtre,
+            'categories' => $categories->findAll(),
+            'produits' => $produits->findAll()
         ]);
     }
 }
