a7176a47841e7a1dcdc155a99ced6446b5aeea2f
Amaniesaid <saidamanie2002@gmail.com>
2023-10-03 13:26:29 +0200

    Modif Nav BAR

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -11,17 +11,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticlesController extends AbstractController
 {
     #[Route('/articles', name: 'app_articles')]
-    public function index(): Response
+    public function index(ProduitsRepository $produits): Response
     {
         return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
+            'controller_name' => 'Tous les articles',
+            'produits' => $produits->findAll(),
+        ]);
+    }
+
+    #[Route('/articles/{id}', name: 'article_details')]
+    public function deatils(int $id, ProduitsRepository $produits): Response
+    {
+        return $this->render('articles/details.html.twig', [
+            'produit' => $produits->find($id),
         ]);
     }
 
     #[Route('/articles/{filtre}', name: 'app_articles_filtre')]
-    public function showArticleFiltre(String $filtre, CategoriesRepository $categories, ProduitsRepository $produits): Response{
+    public function showArticleFiltre(string $filtre, CategoriesRepository $categories, ProduitsRepository $produits): Response
+    {
         return $this->render('articles/articles.html.twig', [
-            'controller_name' => 'ArticlesController',
+            'controller_name' => 'ArticlesControlle',
             'filtre' => $filtre,
             'categories' => $categories->findAll(),
             'produits' => $produits->findAll()
