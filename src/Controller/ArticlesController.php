ed5c1939acc6b914b6a9323ed3f2dad6122cdcbd
thomasdindin <thomas.dindin@gmail.com>
2023-10-03 15:32:26 +0200

    Modif hors cahier des charges

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -10,19 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ArticlesController extends AbstractController
 {
-    #[Route('/articles', name: 'app_articles')]
-    public function index(): Response
-    {
-        return $this->render('articles/index.html.twig', [
-            'controller_name' => 'ArticlesController',
-        ]);
-    }
-
-    #[Route('/articles/{filtre}', name: 'app_articles_filtre')]
-    public function showArticleFiltre(String $filtre, CategoriesRepository $categories, ProduitsRepository $produits): Response{
+    #[Route('/', name: 'app_articles_home')]
+    public function showArticleFiltre(CategoriesRepository $categories, ProduitsRepository $produits): Response{
         return $this->render('articles/articles.html.twig', [
             'controller_name' => 'ArticlesController',
-            'filtre' => $filtre,
             'categories' => $categories->findAll(),
             'produits' => $produits->findAll()
         ]);
