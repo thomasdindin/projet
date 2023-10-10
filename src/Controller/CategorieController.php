cf8366fcc5b42e6d7538ee5ecb5f4f7ac40406df
thomasdindin <thomas.dindin@gmail.com>
2023-10-10 16:06:16 +0200

    Page affichage catégorie

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RayonRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categorie/{id}', name: 'app_categorie')]
+    public function index(int $id, RayonRepository $rayonRepository): Response
+    {
+        $rayon = $rayonRepository->find($id);
+
+        $inRange = [];
+        $min = $_GET['min'] ?? 0;
+        $max = ($_GET['max'] ?? 100) ? null : 100 ;
+
+        foreach ($rayon-> getProduits() as $produit) {
+            if ($produit->getPrix() >= $min && $produit->getPrix() <= $max) {
+                $inRange[] = $produit;
+            }
+        }
+
+        return $this->render('categorie/index.html.twig', [
+            'controller_name' => 'CategorieController',
+            'rayon' => $rayon,
+            'inRange' => $inRange
+        ]);
+    }
+}
